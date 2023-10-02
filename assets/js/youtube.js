5a5f0c1aadc72a813fd097b60b7b483d8d5b8af0
Owen <owenmccr@gmail.com>
2023-10-02 17:46:28 -0400

    removed css file and added bulma + yt thumbnails

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -4,42 +4,80 @@ var searchForm = document.querySelector("#search-form");
 var results = document.querySelector("#results");
 
 searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchQuery = searchInput.value.trim();
+  var searchQuery = searchInput.value.trim();
 
-    if (searchQuery) {
-        getSearchResults(searchQuery);
+  if (searchQuery) {
+    getSearchResults(searchQuery);
 
-        searchInput.value = "";
-    } else {
-        alert("Please enter a valid search!");
-    }
-})
+    searchInput.value = "";
+  } else {
+    alert("Please enter a valid search!");
+  }
+});
 
 function getSearchResults(input) {
-    var apiUrl = "https://www.googleapis.com/youtube/v3/search?q=" + input + "&part=snippet" + "&type=video&key=AIzaSyCQJvOLH9jBWq_H_heswP8ew3OEFU99560";
+  var apiUrl =
+    "https://www.googleapis.com/youtube/v3/search?q=" +
+    input +
+    "&part=snippet" +
+    "&type=video&key=AIzaSyCQJvOLH9jBWq_H_heswP8ew3OEFU99560";
 
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            console.log(data);
-
-            var videoUrl = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
-            var videoTitle = data.items[0].snippet.title
-
-            var resultsEl = document.createElement('a');
-            resultsEl.setAttribute('href', videoUrl);
-            
-            var titleEl = document.createElement('span');
-            titleEl.textContent = videoTitle;
-        
-            resultsEl.appendChild(titleEl);
-
-            results.appendChild(resultsEl);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        showResults(data);
       });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+}
+
+function showResults(data) {
+  console.log(data);
+  data.items.forEach((searchResults) => {
+    var videoUrl =
+      "https://www.youtube.com/watch?v=" + searchResults.id.videoId;
+    var videoTitle = searchResults.snippet.title;
+    var videoDescription = searchResults.snippet.description;
+
+    var thumbnailUrl = searchResults.snippet.thumbnails.default.url
+
+    var article = document.createElement("article");
+    article.classList.add("media")
+
+    var thumbnailEl = document.createElement("figure");
+    thumbnailEl.classList.add("media-left")
+
+    var thumbnailImg = document.createElement("p");
+    thumbnailImg.classList.add("image", "is-64x64");
+
+    var thumbnail = document.createElement("img");
+    thumbnail.setAttribute("src", thumbnailUrl);
+
+    var mediaContent = document.createElement("div");
+    mediaContent.classList.add("media-content");
+
+    var content = document.createElement("div");
+    content.classList.add("content");
+
+    var resultsEl = document.createElement("a");
+    resultsEl.setAttribute("href", videoUrl);
+
+    var videoInfo = document.createElement("p");
+    videoInfo.innerHTML = "<strong>" + videoTitle + "</strong>" + "<br />" + videoDescription;
+
+    results.appendChild(resultsEl);
+    resultsEl.appendChild(article);
+    
+    article.appendChild(thumbnailEl);
+    thumbnailEl.appendChild(thumbnailImg);
+    thumbnailImg.appendChild(thumbnail);
+
+    article.appendChild(mediaContent);
+    mediaContent.appendChild(content);
+    content.appendChild(videoInfo);
+  });
 }
