af80772ee801f1f6e10390dcd229ae9c7d14a268
Owen <owenmccr@gmail.com>
2023-10-02 20:41:48 -0400

    added embedded video player and modals

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -12,6 +12,7 @@ searchForm.addEventListener("submit", (event) => {
     getSearchResults(searchQuery);
 
     searchInput.value = "";
+    results.innerHTML = "";
   } else {
     alert("Please enter a valid search!");
   }
@@ -22,7 +23,7 @@ function getSearchResults(input) {
     "https://www.googleapis.com/youtube/v3/search?q=" +
     input +
     "&part=snippet" +
-    "&type=video&key=AIzaSyCQJvOLH9jBWq_H_heswP8ew3OEFU99560";
+    "&type=video&maxResults=20&key=AIzaSyCQJvOLH9jBWq_H_heswP8ew3OEFU99560";
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
@@ -42,14 +43,15 @@ function showResults(data) {
       "https://www.youtube.com/watch?v=" + searchResults.id.videoId;
     var videoTitle = searchResults.snippet.title;
     var videoDescription = searchResults.snippet.description;
-
-    var thumbnailUrl = searchResults.snippet.thumbnails.default.url
+    var thumbnailUrl = searchResults.snippet.thumbnails.default.url;
+    var videoId = searchResults.id.videoId;
 
     var article = document.createElement("article");
-    article.classList.add("media")
+    article.classList.add("media");
+    article.style.cursor = "pointer";
 
     var thumbnailEl = document.createElement("figure");
-    thumbnailEl.classList.add("media-left")
+    thumbnailEl.classList.add("media-left");
 
     var thumbnailImg = document.createElement("p");
     thumbnailImg.classList.add("image", "is-64x64");
@@ -63,15 +65,16 @@ function showResults(data) {
     var content = document.createElement("div");
     content.classList.add("content");
 
-    var resultsEl = document.createElement("a");
-    resultsEl.setAttribute("href", videoUrl);
+    // var resultsEl = document.createElement("a");
+    // resultsEl.setAttribute("href", videoUrl);
 
     var videoInfo = document.createElement("p");
-    videoInfo.innerHTML = "<strong>" + videoTitle + "</strong>" + "<br />" + videoDescription;
+    videoInfo.innerHTML =
+      "<strong>" + videoTitle + "</strong>" + "<br />" + videoDescription;
 
-    results.appendChild(resultsEl);
-    resultsEl.appendChild(article);
-    
+    // results.appendChild(resultsEl);
+    results.appendChild(article);
+
     article.appendChild(thumbnailEl);
     thumbnailEl.appendChild(thumbnailImg);
     thumbnailImg.appendChild(thumbnail);
@@ -79,5 +82,83 @@ function showResults(data) {
     article.appendChild(mediaContent);
     mediaContent.appendChild(content);
     content.appendChild(videoInfo);
+
+    article.addEventListener("click", function () {
+      document.getElementById("modal1").classList.add("is-active");
+
+      var modalTitle = document.querySelector("#modalTitle");
+      modalTitle.innerHTML = videoTitle;
+
+      var ytplayer = document.querySelector("#player");
+      ytplayer.setAttribute("src", "http://www.youtube.com/embed/" + videoId);
+
+      var tag = document.createElement("script");
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName("script")[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player("player", {
+          playerVars: {
+            playsinline: 1,
+          },
+          events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange,
+          },
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+    });
   });
 }
+
+// Function to close the modal
+function closeModal() {
+  document.getElementById("modal1").classList.remove("is-active");
+}
+
+// Add event listeners to close the modal
+// whenever user click outside modal
+document
+  .querySelectorAll(
+    ".modal-background, .modal-close, .modal-card-head.delete, .modal-card-foot.button"
+  )
+  .forEach(($el) => {
+    const $modal = $el.closest(".modal");
+    $el.addEventListener("click", () => {
+      // Remove the is-active class from the modal
+      $modal.classList.remove("is-active");
+    });
+  });
+
+// Adding keyboard event listeners to close the modal
+document.addEventListener("keydown", (event) => {
+  const e = event || window.event;
+  if (e.keyCode === 27) {
+    // Using escape key
+    closeModal();
+  }
+});
