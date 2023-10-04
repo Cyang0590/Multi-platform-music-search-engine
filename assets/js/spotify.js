45a3f9544c89bbc111e6a3a9c9af8191f01605d5
Chia <yangchai2450@gmail.com>
2023-10-03 21:14:45 -0400

    added the fetch for artist and playlist

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -4,34 +4,65 @@ var searchForm = document.querySelector("#search-form");
 var results = document.querySelector("#results");
 
 
+
 var clientId = "2383905db5474844bcb1768d0aa73893";
 var clientSecret = "798793c082c84d74a10e63bb7d6d604a"
 
 var Url = "https://accounts.spotify.com/api/token"
 
- searchEl= (event) => {
-    event.preventDefault();
-    mySeacrh = searchInput.val()
-    if (mySeacrh === null) {
-        alert("Please enter a valid search!");
-    };
 
-fetch(Url , {
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  var searchQuery = searchInput.value.trim();
+
+  if (searchQuery) {
+    retreiveToken(searchQuery);
+
+    searchInput.value = "";
+    results.innerHTML = "";
+    // } else {
+    //   alert("Please enter a valid search!");
+  }
+});
+
+async function retreiveToken() {
+
+  const result = await fetch(Url, {
     method: 'POST',
     body: 'grant_type=client_credentials&client_id=' + clientId + '&client_secret=' + clientSecret,
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
-.then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        var accessToken = data.access_token
-      });
-    } 
-  });
+      'Content-Type': 'application/x-www-form-urlencoded'
 
+    }
+  })
+  const data = await result.json();
+  access_token = data.access_token;
+  retreiveKeyPlaylist(access_token)
+};
+
+
+
+
+async function retreiveKeyPlaylist(access_token, searchInput) {
+
+  const result = await fetch(`https://api.spotify.com/v1/search?type=track,artist&q=` + searchInput, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ` + access_token
+
+    }
+  })
+  const data = await result.json();
+  console.log(data);
+  showSearchResults(data);
+};
+
+function showSearchResults(data) {
 
 }
-  searchInput.addEventListener("click", searchEl)
+
+
+
+
