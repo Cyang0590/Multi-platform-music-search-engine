500740bd470df75355cbbe45e0a3472e4ce390bc
Owen <owenmccr@gmail.com>
2023-10-09 02:08:47 -0400

    implementing Joe's styling through Bulma

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -310,4 +310,14 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
+
 displaySearchHistory();
